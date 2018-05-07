4633aa1b721de953a09fbc4066dbcbf647fdedbf
KushibikiMashu <kushibiki3413@gmail.com>
2018-05-07 22:43:25 +0900

    [feature/no-ajax]html作成準備

--- no-ajax/chart.php
+++ no-ajax/chart.php
@@ -25,45 +25,6 @@ require_once('index.php');
 
 <body>
 
-  <div class="py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-
-          <ul class="nav nav-pills">
-            <li class="nav-item" id="day">
-              <a href="" class="active nav-link" data-toggle="pill" data-target="#tabone">day</a>
-            </li>
-            <li class="nav-item" id="week">
-              <a class="nav-link" href="#tabtwo" data-toggle="pill" data-target="#tabtwo">weeekly</a>
-            </li>
-            <li class="nav-item"  id="month">
-              <a href="" class="nav-link" data-toggle="pill" data-target="#tabthree">monthly</a>
-            </li>
-          </ul>
-
-          <div class="tab-content mt-2">
-            <div class="tab-pane fade show active" id="tabone" role="tabpanel">
-              <canvas id="my_chart_1">
-                Canvas not supported...
-              </canvas>
-            </div>
-            <div class="tab-pane fade" id="tabtwo" role="tabpanel">
-              <canvas id="my_chart_2">
-                Canvas not supported...
-              </canvas>
-            </div>
-            <div class="tab-pane fade" id="tabthree" role="tabpanel">
-              <canvas id="my_chart_3">
-                Canvas not supported...
-              </canvas>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
 <canvas id="display1">
   Canvas not supported...
 </canvas>
