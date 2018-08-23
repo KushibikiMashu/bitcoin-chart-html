44a5a1537a92cce3a8cbfa43728c2c0d253187f5
KushibikiMashu <kushibiki3413@gmail.com>
2018-08-23 13:00:11 +0900

    [develop]変数名の変更。コード整形

--- public_html/js/database.php
+++ public_html/js/database.php
@@ -1,83 +1,67 @@
 <?php
 
-header('Content-Type: text/html; charset=UTF-8');
-
 // ビットコインの現在価格をデータベースから取得する
-try
-{
-$dsn='mysql:host=127.0.0.1;dbname=newbitDB;charset=utf8';
-$user='root';
-$password='root';
-$pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
-}
-catch (Exception $e)
-{
+try {
+	// DBはローカルのMySQLを指定する
+	$dsn      = 'mysql:host=127.0.0.1;dbname=newbitDB;charset=utf8';
+	$user     = 'root';
+	$password = 'root';
+	$pdo      = new PDO($dsn, $user, $password, [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	]);
+} catch (Exception $e) {
 	print 'データーベース接続エラー発生';
-	print '現在、復旧作業中です。しばらくお待ちください。';
+	print '復旧作業中です。しばらくお待ちください。';
 	exit();
 }
 
-// 最高値のデータ（カラム名をキーにした連想配列）	
-$sql = 'SELECT * FROM bitcoin_exchange';
-
+// カラム名をキーにした連想配列で最高値のデータを取得	
+$sql  = 'SELECT * FROM bitcoin_exchange';
 $stmt = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-$zaif_prices 		= [];
-$bitflyer_prices 	= [];
-$coincheck_prices 	= [];
-$datetime 			= [];
+// 取引所ごとに金額を取得し、配列に格納
+$zaif     = $bitflyer = $coincheck = [];
+$datetime = [];
 
-
-// 1時間ごとの取得
 foreach ($stmt as $key => $value) {
-	$rest		= $value['id'] % 3;
-	$price		= $value['price'];
-	$created_at	= $value['created_at'];
+	$mod        = $value['id'] % 3;
+	$price      = $value['price'];
+	$created_at = $value['created_at'];
 
-	switch ($rest) {
+	switch ($mod) {
+		case '0': // coincheck
+			$coincheck[] = $price;
+			$datetime[]  = substr($created_at, 0, - 1) . '0';
+			break;
+
 		case '1': // zaif
-			$zaif_prices[]	= $price;
+			$zaif[] = $price;
 			break;
 
 		case '2': // bitflyer
-			$bitflyer_prices[]	= $price;
-			break;
-		
-		case '0': // coincheck
-			$coincheck_prices[]	= $price;
-			// $datetime[] = $created_at;
-			$datetime[] = substr($created_at, 0, -1) . '0';
-			
+			$bitflyer[] = $price;
 			break;
 
 		default:
-			
 			break;
 	}
 }
 
-$timestamp = [];
-$json_array = [];
+/*
+ * 配列を作成し、highchartsライブラリが読み取るJSONの形式に合わせて時間と金額を格納
+ * [ [タイムスタンプ, 金額], [タイムスタンプ, 金額]... ]
+ */
+$array_zaif = $array_bitflyer = $array_coincheck = [];
+$timestamp  = [];
 
-// highchartsの形式に合わせる
-// [ [タイムスタンプ, 金額], [タイムスタンプ, 金額]... ]
-for($i=0, $len = count($datetime); $i<$len; $i++){
-
-	$timestamp[] = strtotime($datetime[$i]) . '000';
-
-	$zaif_array[]		 = [(int)$timestamp[$i], (int)$zaif_prices[$i]];
-	$bitflyer_array[] 	 = [(int)$timestamp[$i], (int)$bitflyer_prices[$i]];
-	$coincheck_array[] 	 = [(int)$timestamp[$i], (int)$coincheck_prices[$i]];
+for ($i = 0; $i < count($datetime); $i ++) {
+	// HACK 桁数を揃えるPHPの関数がある
+	$timestamp[]       = strtotime($datetime[$i]) . '000';
+	$array_zaif[]      = [(int)$timestamp[$i], (int)$zaif[$i]];
+	$array_bitflyer[]  = [(int)$timestamp[$i], (int)$bitflyer[$i]];
+	$array_coincheck[] = [(int)$timestamp[$i], (int)$coincheck[$i]];
 }
 
-// 配列をjsonに変換
-$zaif_array 		= json_encode($zaif_array);
-$bitflyer_array		= json_encode($bitflyer_array);
-$coincheck_array 	= json_encode($coincheck_array);
-
-// jsonに出力する
-file_put_contents('json/zaif.json', $zaif_array);
-file_put_contents('json/bitflyer.json', $bitflyer_array);
-file_put_contents('json/coincheck.json', $coincheck_array);
+file_put_contents('json/zaif.json', json_encode($array_zaif));
+file_put_contents('json/bitflyer.json', json_encode($array_bitflyer));
+file_put_contents('json/coincheck.json', json_encode($array_coincheck));
--- public_html/js/multi.js
+++ public_html/js/multi.js
@@ -1,10 +1,10 @@
-// チャートのデータが古い、または読み込めない場合は、
-// ajaxのjson取得アドレスをサーバーに応じて変更する
-
+/*
+ * チャートのデータが古い、または読み込めない場合は、
+ * ajaxのjson取得アドレスをサーバーに応じて変更する
+ */
 var seriesOptions = [],
     seriesCounter = 0,
-    names = ['Zaif', 'bitflyer', 'coincheck'];
-
+    names = ['zaif', 'bitflyer', 'coincheck'];
 
 /**
  * Create the chart when all data is loaded
@@ -14,79 +14,75 @@ function createChart() {
 
     Highcharts.stockChart('container', {
 
-      chart: {
-        height: 525,
-      },
-
-      title: {
-        text: 'Bitcoin price of main exchanges'
-      },
-
+        chart: {
+            height: 525,
+        },
+        title: {
+            text: 'Bitcoin price of main exchanges'
+        },
         rangeSelector: {
             allButtonsEnabled: false,
-            selected: false,
-            buttons : [{
-                type : 'minute', // 分単位 (0)
-                count : 1440,     // 単位は分
-                text : '1d'       // 一日分
+            selected: 0,
+            buttons: [{
+                type: 'minute', // 分単位 (0)
+                count: 1440,     // 単位は分
+                text: '1d'       // 一日分
             }, {
-                type : 'minute',    // 日単位 (1)
-                count : 1440 * 7,   // 1週間分
-                text : '7d'
+                type: 'minute',    // 日単位 (1)
+                count: 1440 * 7,   // 1週間分
+                text: '7d'
             }, {
-                type : 'minute',    // 日単位 (1)
-                count : 1440 * 7 * 2,   // 2週間分
-                text : '2w'
-            },  {
-                type : 'all',    // 全データ
-                count : 1,
-                text : 'All'
+                type: 'minute',    // 日単位 (2)
+                count: 1440 * 7 * 2,   // 2週間分
+                text: '2w'
+            }, {
+                type: 'all',    // 全データ(3)
+                count: 1,
+                text: 'All'
             }]
         },
-
         yAxis: {
             plotLines: [{
                 value: 0,
                 width: 2,
-                color: 'white'
-            }]
+                color: 'white',
+            }],
+            opposite: false, // Y軸の位置
         },
-
-      legend: {
-        enabled: true
-      },
-
+        legend: {
+            enabled: true
+        },
         plotOptions: {
             series: {
                 compare: 'price',
                 showInNavigator: true
             },
         },
-
         tooltip: {
             pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y:,1f}</b><br/>',
             valueDecimals: 0,
             split: true,
         },
-
         series: seriesOptions
     });
 }
 
-// カンマ区切り
+// 数字の桁をカンマ区切るにする
 Highcharts.setOptions({
     global: {
         useUTC: false
     },
-  lang: {
-    // 桁区切りの文字を指定
-    thousandsSep: ','
-  }
+    lang: {
+        // 桁区切りの文字をカンマに指定
+        thousandsSep: ',',
+        numericSymbols: null
+    }
 });
 
+// JSONを読み込む
 $.each(names, function (i, name) {
 
-    $.getJSON('http://bitcoin-chart.info/js/json/' + name.toLowerCase() + '.json',    function (data) {
+    $.getJSON('http://bitcoin-chart.info/js/json/' + name + '.json', function (data) {
 
         seriesOptions[i] = {
             name: name,
